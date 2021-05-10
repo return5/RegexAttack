e2e8034f25398d279a7d62658cc3f62f1eedae89
chris <armoredmoose@gmail.com>
2021-05-10 09:43:21 -0500

    added stuff to main

--- main.lua
+++ main.lua
@@ -9,7 +9,26 @@ function love.update(dt)
 end
 
 
+
+local function initConstants(difficulty)
+    local ship_img = love.graphics.newImage('assets/graphics/ships/ship_1')
+    
+    CONSTANTS = readOnlyTable({
+        WIDTH       = ,
+        HEIGHT      = ,
+        SHIP_LIMIT  = getShipLimit(difficulty),
+        SHIP_HEIGHT = ship_imge:getHeight(),
+        SHIP_WIDTH  = ship_img:getWidth(),
+        DIFFICULTY  = difficulty,
+        OFFSET      = 15,
+        
+        })
+end
+
 function love.load()
+    SHIPS         = {}
+    PLAYER_HEALTH = 100
+    CONSTANTS      = {}
 
 end
 
